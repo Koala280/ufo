5212494725c6dfadaeaddd0e52f6789395a7d861
Lipton3651 <tomsbuss99@gmail.com>
2020-07-22 16:47:02 +0200

    Fixed deleting+spawning Asteroids

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -11,14 +11,13 @@ public class Asteroid : MonoBehaviour {
     // Use this for initialization
     void Start () {
         rb = this.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(-speed, 0);
+        rb.velocity = new Vector2(0, -speed);
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-
     }
 
     // Update is called once per frame
     void Update () {
-        if(transform.position.y < screenBounds.y * 2){
+        if(transform.position.y * 2 < screenBounds.y){
             Destroy(this.gameObject);
         }
     }
